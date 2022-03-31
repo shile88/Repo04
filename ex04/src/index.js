13ca2cf99cab2eb44a1dd04367b3d493e8236e2a
shile88 <milos.ivanis@gmail.com>
2022-03-31 10:49:49 +0200

    Decimal numbers and e

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+
+    var myDecimal = 6.5;
+    var myDecimalE = 254e-5;
+
+
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
